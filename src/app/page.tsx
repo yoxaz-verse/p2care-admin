b4f86e441d47bb8eb5dde027ce6eb82fef497f0c
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-07-19 18:35:12 +0530

    Login changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col shadow-xl justify-center items-center bg-[#F4F4F5] h-[100vh]">
-        <form className="flex flex-col gap-2 items-center shaodw-xl w-[50vh] md:w-[70vh] border border-black bg-white p-[.5rem] md:p-[1rem] rounded-xl">
+        <form className="flex flex-col gap-2 items-center shaodw-xl w-[50vh] md:w-[80vh]  bg-white p-[.5rem] md:p-[1rem] rounded-xl">
           <Image src={adminLogo} alt="adminLogo" width={100} height={100} />
-          <Input placeholder="email" label="Email" labelPlacement="outside" />
-          <Input placeholder="password" label="Password" labelPlacement="outside" />
+          <Input placeholder="Email" label="Email" labelPlacement="outside" />
+          <Input placeholder="Password" label="Password" labelPlacement="outside" />
           <Button color="primary" className="text-[18px] w-full" onClick={() => handleNaviagtion()}>Submit</Button>
         </form>
       </div>
