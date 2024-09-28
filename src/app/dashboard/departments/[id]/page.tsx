0bd97dabcd67c9234063f38093e9cc9668c83567
AlbinJo5 <albinjo12345alex@gmail.com>
2024-09-28 22:53:25 +0530

    latest

--- src/app/dashboard/departments/[id]/page.tsx
+++ src/app/dashboard/departments/[id]/page.tsx
@@ -2,17 +2,29 @@
 import { useState, useEffect } from "react";
 import DeleteModal from "@/components/Modals/DeleteModal";
 import Title from "@/components/titles";
-import { BreadcrumbItem, Breadcrumbs, Button, Card, CardBody, CardHeader, Input, Spinner, Textarea, useDisclosure } from "@nextui-org/react";
+import {
+  Avatar,
+  BreadcrumbItem,
+  Breadcrumbs,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Spinner,
+  Textarea,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useParams, useRouter, usePathname } from "next/navigation";
 import { Doctor, HospitalRoutes } from "@/core/apiRoutes";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { getData, patchData } from "@/core/apiHandler";
+import { getData, patchData, postMultipart } from "@/core/apiHandler";
 import { queryAdmin } from "@/app/providers";
 import Page from "@/components/Page/PageAll";
 import { useAsyncList } from "@react-stately/data";
 import { ImageSingle } from "@/components/ImageUpload";
-
-
+import { toast } from "sonner";
+import { uploadLogo } from "@/content/assets";
 
 export default function DepartmentDetails() {
   const router = useRouter();
@@ -24,7 +36,12 @@ export default function DepartmentDetails() {
     { name: "Department", link: "/dashboard/departments" },
     { name: "Department Details", link: path },
   ];
-  const { data: getDepartment, isLoading, isError, isFetched } = useQuery({
+  const {
+    data: getDepartment,
+    isLoading,
+    isError,
+    isFetched,
+  } = useQuery({
     queryKey: ["getDepartment"],
     queryFn: () => getData(`${Doctor.department}/${id}`, {}),
   });
@@ -38,7 +55,7 @@ export default function DepartmentDetails() {
   });
 
   useEffect(() => {
-    console.log(getDepartment?.data.data)
+    console.log(getDepartment?.data.data);
     if (getDepartment?.data?.data) {
       setFormData({
         name: getDepartment.data.data.name,
@@ -53,12 +70,13 @@ export default function DepartmentDetails() {
     queryKey: ["get-prodcedure"],
     queryFn: () => {
       return getData(Doctor.procedure, {});
-    }
-  })
+    },
+  });
 
   const updateData = useMutation({
     mutationKey: ["update-department"],
-    mutationFn: (data: any) => patchData(`${Doctor.department}/${id}`, data, {}),
+    mutationFn: (data: any) =>
+      patchData(`${Doctor.department}/${id}`, data, {}),
     onSuccess: (data: any) => {
       console.log(data.data);
       queryAdmin.invalidateQueries({ queryKey: ["getDepartment"] });
@@ -68,7 +86,9 @@ export default function DepartmentDetails() {
     },
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prev: any) => ({
       ...prev,
@@ -76,52 +96,103 @@ export default function DepartmentDetails() {
     }));
   };
 
-
   const procedureColumns = [
     { name: "Name", uid: "name", type: "text" },
     { name: "Department", uid: "department", type: "departmentDropdown" },
     {
-      name: "Actions", uid: "actions", type: "actions"
-    }
-  ]
+      name: "Actions",
+      uid: "actions",
+      type: "actions",
+    },
+  ];
   const list1 = useAsyncList<any>({
     async load() {
       let res = await getData(Doctor.department, {});
       let json = await res.data.data.data;
 
       return {
-        items: json
+        items: json,
       };
     },
   });
   const DropDownData = {
-    "department": list1
-  }
+    department: list1,
+  };
   const enquiryColumns = [
     { name: "Name", uid: "name", type: "text" },
     { name: "Phone", uid: "phoneno", type: "text" },
     { name: "Email", uid: "email", type: "text" },
     {
-      name: "Status", uid: "status", type: "enquirystatus"
+      name: "Status",
+      uid: "status",
+      type: "enquirystatus",
     },
     {
-      name: "Actions", uid: "actions", type: "actions"
-    }
-  ]
+      name: "Actions",
+      uid: "actions",
+      type: "actions",
+    },
+  ];
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     updateData.mutate(formData);
   };
   const [isEdit, setisEdit] = useState<boolean>(false);
+
+  const addImage = useMutation({
+    mutationKey: ["addBanner-Department"],
+    mutationFn: (data: any) => {
+      return postMultipart(`/department/upload/banner/${id}`, {}, data);
+    },
+    onSuccess: (data: any) => {
+      toast.success("Banner is uploaded successfully", {
+        position: "top-right",
+        className: "bg-green-300",
+      });
+      queryAdmin.invalidateQueries({ queryKey: ["getDepartment"] });
+    },
+    onError: (error: any) => {
+      console.log(error);
+      toast.error(error.response.data, {
+        position: "top-right",
+        className: "bg-green-500",
+      });
+    },
+  });
+
+  const [file, setFile] = useState<File>();
+  const [uploadImageUrl, setUploadImageUrl] = useState<string>(uploadLogo);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      alert("No file uploaded");
+      return;
+    }
+    const file = e.target.files[0];
+    setFile(file);
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setUploadImageUrl(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+    const formData = new FormData();
+    formData.append("image", file);
+    addImage.mutate(formData);
+  };
   return (
     <div className="flex flex-col gap-4 w-full">
-      {isLoading ? <Spinner /> : (
+      {isLoading ? (
+        <Spinner />
+      ) : (
         <div className="flex flex-col w-full gap-4">
           <div className="flex flex-col gap-4 p-[1rem]">
             <Breadcrumbs color="secondary" className="text-xl font-bold">
               {breadCrumps.map((b) => (
-                <BreadcrumbItem key={b.name} onClick={() => router.push(b.link)}>
+                <BreadcrumbItem
+                  key={b.name}
+                  onClick={() => router.push(b.link)}
+                >
                   {b.name}
                 </BreadcrumbItem>
               ))}
@@ -130,15 +201,17 @@ export default function DepartmentDetails() {
             <div className="flex flex-row justify-between items-center w-full">
               <Title title={getDepartment?.data?.data?.name} />
               <div className="flex flex-row gap-4">
-
-                <Button color="primary"
+                <Button
+                  color="primary"
                   radius="full"
-                  onClick={() => setisEdit(true)}>
+                  onClick={() => setisEdit(true)}
+                >
                   Edit
                 </Button>
 
-
-                <Button color="danger" radius="full" onClick={onOpen}>Delete</Button>
+                <Button color="danger" radius="full" onClick={onOpen}>
+                  Delete
+                </Button>
               </div>
             </div>
             <DeleteModal
@@ -151,13 +224,45 @@ export default function DepartmentDetails() {
             />
           </div>
           <Card shadow="sm" radius="lg">
-            <CardHeader className="text-xl font-bold">Department Details</CardHeader>
+            <CardHeader className="text-xl font-bold">
+              Department Details
+            </CardHeader>
             <CardBody className="flex flex-row h-full items-center justify-center">
-              <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
-                <ImageSingle id={id}
-                  getapikey="getDepartment"
-                  postapi={"/department/upload"}
-                  image={getDepartment?.data?.data?.image} />
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-4 w-full"
+              >
+                <div className="flex gap-5">
+                  <ImageSingle
+                    id={id}
+                    getapikey="getDepartment"
+                    postapi={"/department/upload"}
+                    image={getDepartment?.data?.data?.image}
+                  />
+                  <label
+                    htmlFor="departmentBannerUpload"
+                    className="cursor-pointer"
+                  >
+                    <Avatar
+                      src={
+                        getDepartment?.data.data?.banner
+                          ? getDepartment?.data.data?.banner.path
+                          : uploadImageUrl
+                      }
+                      alt="department Banner"
+                      className="w-full h-full rounded-xl"
+                    />
+                    <input
+                      id="departmentBannerUpload"
+                      type="file"
+                      accept="image/*"
+                      className="hidden"
+                      onChange={(e: any) => {
+                        handleChange(e);
+                      }}
+                    />
+                  </label>
+                </div>
                 <Input
                   name="name"
                   value={formData.name}
@@ -191,7 +296,9 @@ export default function DepartmentDetails() {
                   onChange={handleInputChange}
                 />
                 {isEdit && (
-                  <Button type="submit" color="primary" className="text-xl">Submit</Button>
+                  <Button type="submit" color="primary" className="text-xl">
+                    Submit
+                  </Button>
                 )}
               </form>
             </CardBody>
@@ -203,8 +310,15 @@ export default function DepartmentDetails() {
         api={`${Doctor.enquiry}/individual/${id}`}
         apiKey="enquiryByHospital"
         columns={enquiryColumns}
-        title={`${getDepartment?.data?.data?.name} Enquiry`} />
-      <Page dropDownData={DropDownData} api={Doctor.procedure} apiKey="procedure" columns={procedureColumns} title={`${getDepartment?.data?.data?.name} Procedures`} />
+        title={`${getDepartment?.data?.data?.name} Enquiry`}
+      />
+      <Page
+        dropDownData={DropDownData}
+        api={Doctor.procedure}
+        apiKey="procedure"
+        columns={procedureColumns}
+        title={`${getDepartment?.data?.data?.name} Procedures`}
+      />
     </div>
   );
 }
